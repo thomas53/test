14292f9b613ff1ed2cf3726e209b92f45c414574
thomas53 <kent.t4nk@gmail.com>
2016-01-13 21:57:53 +0700

    v1.0.0

---
+++ assets/js/myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay:true
+ 
+  });
+ 
+});
